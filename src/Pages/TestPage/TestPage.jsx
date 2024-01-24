7f833c696a95a936e3ea5a18b44476e56b27266e
Gustavo-daCosta <80484719+Gustavo-daCosta@users.noreply.github.com>
2024-01-24 12:16:00 -0300

    Music Stats: Lógica de requisição de tokens completa

--- src/Pages/TestPage/TestPage.jsx
+++ src/Pages/TestPage/TestPage.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import qs from 'qs';
 import MainContent from '../../Components/MainContent/MainContent';
 import Container from '../../Components/Container/Container';
 import spotifyApi from '../../Services/SpotifyApi';
@@ -9,10 +11,47 @@ const TestPage = () => {
     const [albumName, setAlbumName] = useState("");
     const [image, setImage] = useState("");
     const [artist, setArtist] = useState("");
+    const [authToken, setAuthToken] = useState("");
 
     useEffect(() => {
-        loadArtists();
-    }, [name]);
+        getToken();
+    }, []);
+
+    // useEffect(() => {
+    //     loadArtists();
+    // }, [name]);
+
+    const getToken = async () => {
+        const clientId = process.env.REACT_APP_CLIENT_ID;
+        const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
+    
+        const headers = {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: clientId,
+                password: clientSecret,
+            },
+        };
+    
+        const data = {
+            grant_type: 'client_credentials',
+        };
+    
+        try {
+            const promise = await axios.post(
+                'https://accounts.spotify.com/api/token',
+                qs.stringify(data),
+                headers
+            );
+            console.log(promise.data);
+            setAuthToken(promise.data.access_token);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     async function loadArtists() {
         try {
@@ -28,14 +67,23 @@ const TestPage = () => {
 
     return (
         <MainContent>
-            <Container>
-                <div className="music-card">
-                    <img src={image} alt="" />
-                    <h1>{name}</h1>
-                    <p>{albumName}</p>
-                    <h3 className="artist">{artist}</h3>
-                </div>
-            </Container>
+            <section className="music">
+                <Container>
+                    <div className="music-card">
+                        <img src={image} alt="" />
+                        <h1>{name}</h1>
+                        <p>{albumName}</p>
+                        <h3 className="artist">{artist}</h3>
+                    </div>
+                </Container>
+            </section>
+            <section className="token-test">
+                <Container>
+                    <div className="token-info">
+                        <button onClick={console.log("teste")}>Gerar token</button>
+                    </div>
+                </Container>
+            </section>
         </MainContent>
     );
 };
